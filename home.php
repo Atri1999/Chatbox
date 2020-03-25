2dce1618526e8335aeca9792a4ee13dbd7f56b37
Atri Chakraborty <chakrabortyatri1999@gmail.com>
2020-03-25 17:27:06 +0530

    initial commit just the structure

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css"/>
+    <title>Home</title>
+</head>
+<body>
+    <div class="header">
+        <div>
+            <div class="menu">Logout</div>
+            <div class="menu">Friends</div>
+            <div class="menu">Profile</div> 
+        </div>     
+    </div>
+
+    <div class="middle">
+        <div class="content">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+        </div>
+
+    
+    </div>
+    
+</body>
+</html>
+
